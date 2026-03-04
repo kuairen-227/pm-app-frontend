48a193b0e714c789ad30043cf91b82697dee2cbb
kuairen <kaito_127@icloud.com>
2026-03-04 13:29:19 +0000

    build(package): :wrench: Vitest × React Testing Library に必要な設定、パッケージを追加

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,10 @@ import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {},
+  test: {
+    environment: "jsdom",
+    globals: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
