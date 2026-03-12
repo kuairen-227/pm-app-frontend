56afceec12fd5422bd8e9030ddaf082db48ab80e
kuairen <kaito_127@icloud.com>
2026-03-12 10:05:18 +0000

    refactor(api): :recycle: TanstackQuery の `QueryClient` のファクトリ関数を作成

--- components/providers/query-provider.tsx
+++ components/providers/query-provider.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+import { createQueryClient } from "@/shared/lib/query";
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(createQueryClient);
 
   return (
     <QueryClientProvider client={queryClient}>
