bb5d9de649918de4b9d2c1ccd8924130b93192e7
kuairen <kaito_127@icloud.com>
2026-03-15 16:27:23 +0000

    chore(api): :recycle: 不要な囲い文字削除

--- shared/api/client.ts
+++ shared/api/client.ts
@@ -3,6 +3,6 @@ import { env } from "../config/env";
 import type { paths } from "../types/open-api";
 
 export const apiClient = createClient<paths>({
-  baseUrl: `${env.NEXT_PUBLIC_API_BASE_URL}`,
+  baseUrl: env.NEXT_PUBLIC_API_BASE_URL,
   credentials: "include",
 });
