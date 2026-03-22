bff44208118d026abc6ab060653adabc6e1085ac
kuairen <kaito_127@icloud.com>
2026-03-22 14:50:41 +0000

    feat(project): :clown_face: プロジェクト単体取得、作成 API のモック追加

--- mocks/handlers/project.ts
+++ mocks/handlers/project.ts
@@ -1,5 +1,8 @@
-import { http } from "msw";
-import type { ListProjectsResponse } from "@/features/project/types/api";
+import { HttpResponse, http } from "msw";
+import type {
+  GetProjectResponse,
+  ListProjectsResponse,
+} from "@/features/project/types/api";
 import { mockProjects } from "../data/projects";
 import { getDefaultBaseUrl } from "../utils/apiConfig";
 import { createMockResponse, type MockConfig } from "../utils/mockHelper";
@@ -10,6 +13,13 @@ const listProjectsConfig: MockConfig<ListProjectsResponse> = {
   mode: "success",
   successStatus: 200,
   errorStatus: 401,
+  customResponses: undefined,
+};
+
+const getProjectConfig: MockConfig<GetProjectResponse> = {
+  mode: "success",
+  successStatus: 200,
+  errorStatus: 404,
   customResponses: undefined,
 };
 
@@ -24,4 +34,37 @@ export const listProjectsHandler = http.get(baseUrl, async () => {
     {},
     listProjectsConfig,
   );
+});
+
+/**
+ * プロジェクト単体取得 API
+ */
+export const getProjectHandler = http.get(
+  `${baseUrl}/:id`,
+  async ({ params }) => {
+    const { id } = params;
+    const project = mockProjects.find((p) => p.id === id);
+
+    return createMockResponse(
+      undefined,
+      () => !!project,
+      project,
+      {},
+      getProjectConfig,
+    );
+  },
+);
+
+/**
+ * プロジェクト作成 API
+ */
+export const launchProjectHandler = http.post(baseUrl, async () => {
+  const newProjectId = crypto.randomUUID();
+
+  return new HttpResponse(null, {
+    status: 201,
+    headers: {
+      Location: `${baseUrl}/${newProjectId}`,
+    },
+  });
 });
