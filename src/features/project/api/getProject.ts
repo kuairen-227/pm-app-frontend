c74d0921509fad59d1d5d9942c53527b9cc19719
kuairen <kaito_127@icloud.com>
2026-03-24 11:29:42 +0000

    refactor(project): :label: 型定義をドメイン明示

--- src/features/project/api/getProject.ts
+++ src/features/project/api/getProject.ts
@@ -1,8 +1,9 @@
 import { apiClient } from "@/shared/api/client";
 import { checkApiError, checkApiResponse } from "@/shared/api/error";
 import { DEFAULT_API_VERSION } from "@/shared/config/api";
+import type { Project } from "../types/project";
 
-export async function getProject(projectId: string) {
+export async function getProject(projectId: Project["id"]) {
   const { response, data, error } = await apiClient.GET(
     "/api/v{version}/projects/{projectId}",
     {
