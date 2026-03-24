c6b89cde53d59c982d4b8d208410fe453954c5f1
kuairen <kaito_127@icloud.com>
2026-03-24 11:45:24 +0000

    feat(project): :sparkles: プロジェクト関係のルート追加

--- src/shared/config/route.ts
+++ src/shared/config/route.ts
@@ -4,8 +4,11 @@ export const routes = {
     login: () => "/login",
   },
   project: {
-    detail: (projectId: string) => `/projects/${projectId}`,
     new: () => "/projects/new",
+    detail: (projectId: string) => `/projects/${projectId}`,
+    edit: (projectId: string) => `/projects/${projectId}/edit`,
+    members: (projectId: string) => `/projects/${projectId}/members`,
+    settings: (projectId: string) => `/projects/${projectId}/settings`,
   },
   ticket: {
     list: (projectId: string) => `/projects/${projectId}/tickets`,
